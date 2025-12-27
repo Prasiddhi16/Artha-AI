2e9fcc38d388d5dc9654864648f26c83c856e4b3
Ushma-Sapkota <ushma.sapko@gmail.com>
2025-12-27 20:20:01 +0545

    changes in nav

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+function setActiveNav() {
+  const currentPage = window.location.pathname
+    .split("/")
+    .pop()
+    .toLowerCase();
+
+  const items = document.querySelectorAll(".sidebar-item");
+
+  items.forEach(item => {
+    const href = item.getAttribute("href").toLowerCase();
+    item.classList.toggle("active", href === currentPage);
+  });
+}
+
+fetch("navbar.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("navbar").innerHTML = html;
+    setActiveNav(); 
+  });
