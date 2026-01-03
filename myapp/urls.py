fbd3a34d374ab253a966c727895ccfcb8d096809
Ushma-Sapkota <ushma.sapko@gmail.com>
2026-01-03 12:06:28 +0545

    budget page

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,13 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('analytics/',views.analytics,name='analytics'),
+    path('budget/',views.budget,name='budget'),
     path('review/', views.review, name='review'),
     path('goals/', views.goals, name='goals'),
     path('help/', views.help, name='help'),
     path('profile/', views.profile, name='profile'),
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
-    path('settings/', views.settings, name='settings'),
     path('chatbot/', views.chatbot, name='chatbot'),
 
    
