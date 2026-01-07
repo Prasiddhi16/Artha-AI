1b48a6279169d56bfffdc2a42aa5f29db03b0310
AnushaKhatri5 <anushakhatri878@gmail.com>
2026-01-07 17:00:27 +0545

    Initial commit of Artha-Folder7

---
+++ myapp/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('logout/', views.logout_view, name='logout'),
+    path("forgot-password/", views.forgot_password, name="forgot_password"),
+    path("verify-otp/", views.verify_otp, name="verify_otp"),
+    path("reset-password/", views.reset_password, name="reset_password"),
+    path('goals/', views.goals, name='goals'),
+    path('goal/<int:goal_id>/contribute/', views.add_contribution, name='add_contribution'),
+    path('analytics/', views.analytics, name='analytics'),
+    path('budget/', views.budget_view, name='budget'),
+    path('review/', views.review, name='review'),
+    path('help/', views.help_view, name='help'),
+    path('profile/', views.profile, name='profile'),
+    path('chatbot/', views.chatbot, name='chatbot'),
+    path('ajax/add_contribution/', views.add_contribution_ajax, name='add_contribution_ajax'),
+    path('delete_goal/', views.delete_goal, name='delete_goal'),
+    path("goals/contributions/",views.goal_contributions_ajax,name="goal_contributions_ajax"),
+    path("scan-receipt/", views.scan_receipt, name="scan_receipt"),
+    path('api/chatbot/', views.chatbot_api, name='chatbot_api'),
+
+]
---
+++ myproject/settings.py
@@ -0,0 +1,115 @@
+import os
+from pathlib import Path
+import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# PyMySQL setup for Django MySQL backend
+pymysql.version_info = (2, 2, 1, 'final', 0)
+pymysql.install_as_MySQLdb()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-78(tzg5ghtga(nqa47=q6&z5vg@60=01x6b15qbz#z(ff-h!%+'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',  # Added for frontend connection
+    'myapp', 
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # Added at the top
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'myproject.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'myproject.wsgi.application'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'artha_db',
+        'USER': 'artha_user',
+        'PASSWORD': 'MySQL@1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+# Custom User Model
+AUTH_USER_MODEL = 'myapp.User'
+
+# CORS Configuration
+CORS_ALLOW_ALL_ORIGINS = True # Allows any frontend to connect (local development only)
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'   # or your provider
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_app_password' # use app password, not real password
+
+
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
